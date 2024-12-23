b6c3b94258b2a97d6ea028487d9c60043880be99
Sajitha Saranga <109761136+saranga1998@users.noreply.github.com>
2024-12-23 22:26:51 +0530

    Employe view tabel

--- ReactApi/src/Pages/Employee.jsx
+++ ReactApi/src/Pages/Employee.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddEmp from '../Components/AddEmp';
+import EmployeeView from '../Components/EmployeeView';
+import Home from '../Pages/Home'
+
 
 function Employee() {
-  return (
-    <div>
-      Hello
-    </div>
-  )
+    return (
+        <>
+            <Router>
+                <div>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/add-employee">Add Employee</a></li>
+                            <li><a href="/employees">Employee List</a></li>
+                        </ul>
+                    </nav>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/add-employee" element={<AddEmp />} />
+                        <Route path="/employees" element={<EmployeeView />} />
+                    </Routes>
+                </div>
+            </Router>
+        </>
+    )
 }
 
 export default Employee
