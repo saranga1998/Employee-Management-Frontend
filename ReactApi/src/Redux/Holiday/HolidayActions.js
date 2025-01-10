29f418c4ef9fa04d2d51bb01ea3ff3b7aef6862b
Sajitha Saranga <109761136+saranga1998@users.noreply.github.com>
2025-01-10 13:14:11 +0530

    Holiday API update

--- ReactApi/src/Redux/Holiday/HolidayActions.js
+++ ReactApi/src/Redux/Holiday/HolidayActions.js
@@ -1,4 +1,9 @@
-import { FETCH_HOLIDAY_FAILURE,FETCH_HOLIDAY_SUCCESS,FETCH_HOLIDAY_REQUEST } from "./HolidayTypes";
+import {
+    FETCH_HOLIDAY_FAILURE, FETCH_HOLIDAY_SUCCESS, FETCH_HOLIDAY_REQUEST,
+    ADD_HOLIDAY_REQUEST, ADD_HOLIDAY_SUCCESS, ADD_HOLIDAY_FAILURE,
+    DELETE_HOLIDAY_REQUEST, DELETE_HOLIDAY_SUCCESS, DELETE_HOLIDAY_FAILURE,
+    UPDATE_HOLIDAY_SUCCESS
+} from "./HolidayTypes";
 import holidayApi from "../../actions/holidayApi";
 
 export const fetchHolidayRequest = () => {
@@ -21,6 +26,51 @@ export const fetchHolidayFailure = (error) => {
     };
 };
 
+export const addHolidayRequest = () => {
+    return {
+        type: ADD_HOLIDAY_REQUEST
+    };
+};
+
+export const addHolidaySuccess = (holiday) => {
+    return {
+        type: ADD_HOLIDAY_SUCCESS,
+        payload: holiday
+    };
+}
+
+export const addHolidayFailure = (error) => {
+    return {
+        type: ADD_HOLIDAY_FAILURE,
+        payload: error
+    };
+}
+
+export const deleteHolidayRequest = () => {
+    return {
+        type: DELETE_HOLIDAY_REQUEST
+    };
+}
+export const deleteHolidaySuccess = (holiday) => {
+    return {
+        type: DELETE_HOLIDAY_SUCCESS,
+        payload: holiday
+    };
+}
+export const deleteHolidayFailure = (error) => {
+    return {
+        type: DELETE_HOLIDAY_FAILURE,
+        payload: error
+    };
+}
+
+export const updateHolidaySuccess = (holiday) => {
+    return {
+        type: UPDATE_HOLIDAY_SUCCESS,
+        payload: holiday
+    };
+}
+
 export const fetchAll = () => {
     return (dispatch) => {
         dispatch(fetchHolidayRequest());
@@ -32,6 +82,70 @@ export const fetchAll = () => {
             .catch((error) => {
                 const errMsg = error.message;
                 dispatch(fetchHolidayFailure(errMsg));
+            });
+    };
+};
+
+export const create = (holiday) => {
+    return (dispatch) => {
+        dispatch(addHolidayRequest());
+        holidayApi.holiday().AddHoliday(holiday)
+            .then((response) => {
+                const day = response.data;
+                dispatch(addHolidaySuccess(day));
+            })
+            .catch((error) => {
+                const errMsg = error.message;
+                dispatch(addHolidayFailure(errMsg));
+            });
+    };
+};
+
+export const deleteById = (id) => {
+    return (dispatch) => {
+        dispatch(deleteHolidayRequest());
+        holidayApi.holiday().DeleteHoliday(id)
+            .then((response) => {
+                const day = response.data;
+                dispatch(deleteHolidaySuccess(day));
+                dispatch(fetchAll());
+            })
+            .catch((error) => {
+                const errMsg = error.message;
+                dispatch(deleteHolidayFailure(errMsg));
+            });
+    }
+}
+
+export const FetchById = (id) => {
+    return (dispatch) => {
+        dispatch(fetchHolidayRequest());
+        holidayApi.holiday().FetchByID(id)
+            .then((response) => {
+                const day = response.data;
+                dispatch(fetchHolidaySuccess(day));
+            })
+            .catch((error) => {
+                const err = error.message;
+                dispatch(fetchHolidayFailure(err));
+            });
+    }
+}
+
+export const UpdateById = (id,edit) =>
+    {
+        return (dispatch) => {
+            dispatch(addHolidayRequest());
+            holidayApi.holiday().EditHoliday(id,edit)
+            .then((response) => {
+                const day = response.data;
+                console.log("Res",edit);
+                dispatch(updateHolidaySuccess(day));
+                dispatch(fetchAll());
+            })
+            .catch((error) => {
+                const errMsg = error.message;
+                dispatch(addEmployeeFailure(errMsg));
             });
     };
 };
--- ReactApi/src/Redux/Holiday/HolidayReduser.js
+++ ReactApi/src/Redux/Holiday/HolidayReduser.js
@@ -1,4 +1,9 @@
-import { FETCH_HOLIDAY_FAILURE,FETCH_HOLIDAY_SUCCESS,FETCH_HOLIDAY_REQUEST } from "./HolidayTypes";
+import {
+    FETCH_HOLIDAY_FAILURE, FETCH_HOLIDAY_SUCCESS, FETCH_HOLIDAY_REQUEST,
+    ADD_HOLIDAY_REQUEST, ADD_HOLIDAY_SUCCESS, ADD_HOLIDAY_FAILURE,
+    DELETE_HOLIDAY_REQUEST, DELETE_HOLIDAY_SUCCESS, DELETE_HOLIDAY_FAILURE,
+    UPDATE_HOLIDAY_SUCCESS
+} from "./HolidayTypes";
 
 const initialState = {
     loading: false,
@@ -7,7 +12,7 @@ const initialState = {
 }
 
 const HolidayReducer = (state = initialState, action) => {
-    switch(action.type){
+    switch (action.type) {
         case 'FETCH_HOLIDAY_REQUEST':
             return {
                 ...state,
@@ -16,14 +21,57 @@ const HolidayReducer = (state = initialState, action) => {
         case 'FETCH_HOLIDAY_SUCCESS':
             return {
                 loading: false,
-                employees: action.payload,
+                holiday: action.payload,
                 error: ''
             }
         case 'FETCH_HOLIDAY_FAILURE':
             return {
                 loading: false,
-                employees: [],
+                holiday: [],
                 error: action.payload
+            }
+
+        case 'ADD_HOLIDAY_REQUEST':
+            return {
+                ...state,
+                loading: true
+            }
+        case 'ADD_HOLIDAY_SUCCESS':
+            return {
+                ...state,
+                loading: false,
+                holiday: [...state.employees, action.payload]
+            }
+
+        case 'ADD_HOLIDAY_FAILURE':
+            return {
+                loading: false,
+                holiday: [],
+                error: action.payload
+            }
+
+        case 'DELETE_HOLIDAY_REQUEST':
+            return {
+                ...state,
+                loading: true
+            }
+        case 'DELETE_HOLIDAY_SUCCESS':
+            return {
+                ...state,
+                loading: false,
+                holiday: state.holiday.filter(holiday => holiday.holidayId !== action.payload)
+            }
+        case 'DELETE_HOLIDAY_FAILURE':
+            return {
+                loading: false,
+                holiday: [],
+                error: action.payload
+            }
+        case 'UPDATE_HOLIDAY_SUCCESS':
+            return {
+                ...state,
+                loading: false,
+                holiday: state.holiday.map(holiday => holiday.holidayId === action.payload.holidayId ? action.payload : holiday)
             }
         default: return state;
     }
