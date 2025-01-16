98e3588af1dc0acac544e441fc06f6d3a2869d8c
Sajitha Saranga <109761136+saranga1998@users.noreply.github.com>
2025-01-16 21:43:24 +0530

    Login Token

--- ReactApi/src/Pages/Authentication/Login.jsx
+++ ReactApi/src/Pages/Authentication/Login.jsx
@@ -1,22 +1,48 @@
-import React, { useState } from 'react'
+import React,{ useState,useEffect }from 'react'
 import { PasswordInput, TextInput } from '../../Components/Inputs/BasicInputs'
+import { useSelector, useDispatch } from 'react-redux'
+import { login } from '../../Redux/User/UserActions'
+import { useNavigate } from 'react-router-dom'
 
 function Login() {
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();  
+  const { user, error, loading } = useSelector((state) => state.users);
+  const [logError,setLogError]=useState();
   const [credential, setCredential] = useState({
     username: '',
     password: '',
-    isLogin: false,
-    isEmpty: false,
-    error: ''
-  })
+    error: '',
+  });
 
-  const handleChange =(e) => {
-    setCredential({...credential,[e.target.name]: e.target.value })
+  const handleChange = (e) => {
+    setCredential({ ...credential, [e.target.name]: e.target.value })
   }
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault();
+    if (!credential.username || !credential.password) {
+      setCredential((prevState) => ({
+        ...prevState,
+        error: 'Please fill in both username and password.',
+      }));
+      return;
+    }
+    else{
+      dispatch(login(credential));
+      if(user){
+        console.log("Token",user)
+        localStorage.setItem('Token', user.token);
+        navigate('/'); 
+      }
+      else{
+        setLogError('Log error');
+      }
+    }
+    
   }
+  
+ 
   return (
     <div>
       <form onSubmit={handleSubmit}>
@@ -25,11 +51,12 @@ function Login() {
 
         <PasswordInput
           label="Password" name="password" type="password" value={credential.password} onChange={handleChange} />
-
+        {credential.error && <p style={{ color: 'red' }}>{credential.error}</p>}
+        {error && <p style={{ color: 'red' }}>{error}</p>}
         <button type='submit'>Login</button>
       </form>
     </div>
   )
 }
 
-export default Login
+export default Login;
